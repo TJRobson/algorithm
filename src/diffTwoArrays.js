a9189f4aceb1b85096740b390e4cff44b2e3e3ed
Guanzhou Zhao <benzhao2866@gmail.com>
2016-11-05 18:11:06 +1300

    Work out diffTwoArrays

--- src/diffTwoArrays.js
+++ src/diffTwoArrays.js
@@ -1,5 +1,38 @@
 export default function diffArray(arr1, arr2) {
   const newArr = [];
   // Same, same; but different.
+  if (!arr1 || !arr2) {
+    return [];
+  }
+  let index1 = 0;
+  let index2 = 0;
+  while (index1 < arr1.length || index2 < arr2.length) {
+    if (index1 === arr1.length) {
+      newArr.push(arr2[index2]);
+      index2 += 1;
+    } else if (index2 === arr2.length) {
+      newArr.push(arr1[index1]);
+      index1 += 1;
+    } else if (arr1[index1] !== arr2[index2]) {
+      let foundIndex1InArr1 = false;
+      const tempArr = [arr2[index2]];
+      for (let i = index2 + 1; !foundIndex1InArr1 && i < arr2.length; i += 1) {
+        if (arr1[index1] === arr2[i]) {
+          newArr.push(...tempArr);
+          foundIndex1InArr1 = true;
+          index2 = i;
+        } else {
+          tempArr.push(arr2[i]);
+        }
+      }
+      if (!foundIndex1InArr1) {
+        newArr.push(arr1[index1]);
+        index1 += 1;
+      }
+    } else if (arr1[index1] === arr2[index2]) {
+      index1 += 1;
+      index2 += 1;
+    }
+  }
   return newArr;
 }
