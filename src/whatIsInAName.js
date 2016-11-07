b9564b841c4f62171e839928eea34df95bac2b22
Guanzhou Zhao <benzhao2866@gmail.com>
2016-11-07 21:41:47 +1300

    add Challenge of whatIsInName

---
+++ src/whatIsInAName.js
@@ -0,0 +1,9 @@
+export default function whatIsInAName(collection, source) {
+  // What's in a name?
+  const arr = [];
+  // Only change code below this line
+
+
+  // Only change code above this line
+  return arr;
+}
