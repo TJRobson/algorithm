3ac1ba49c16b5f7f9281d5b656e352f8e6b64817
guanzhou-zhao <benzhao2866@gmail.com>
2016-12-04 23:05:25 +0800

    Pass convertHTML

--- src/intermediate/convertHTML.js
+++ src/intermediate/convertHTML.js
@@ -1,4 +1,16 @@
 export default function convertHTML(str) {
   // &colon;&rpar;
-  return str;
+  var map = {
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '"': '&quot;',
+    '\'': '&apos;'
+  };
+  
+  return str.replace(/[&<>"']/g, function(c) { console.log(c); return map[c]; });
+  //var result = str.replace('&', '&amp;').replace(/</g, '&lt;').replace('>', '&gt;').replace(/"/g, '&quot;').replace(/'/g, '&apos;');
+  
+  
+  //return result;return str;
 }
--- test/intermediate/addTogether.test.js
+++ test/intermediate/addTogether.test.js
@@ -1,7 +1,7 @@
 import { assert } from 'chai';
 import addTogether from '../../src/intermediate/addTogether.js';
 
-describe.only('Arguments Optional', () => {
+describe('Arguments Optional', () => {
   it('addTogether(2, 3) should return 5.', () => {
     assert.deepEqual(addTogether(2, 3), 5, 'message: <code>addTogether(2, 3)</code> should return 5.');
   });
