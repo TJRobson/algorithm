48e24bc0513817f8493847dc60e2d59d750527ea
Ben Zhao <benzhao2866@gmail.com>
2016-12-13 22:00:28 +1300

    Work on spinal case

--- src/intermediate/spinalCase.js
+++ src/intermediate/spinalCase.js
@@ -1,6 +1,6 @@
 export default function spinalCase(str) {
   // "It's such a fine line between stupid, and clever."
   // --David St. Hubbins
-  console.log(str.replace(/(\s|_|-|A-Z)?(\w+)/g, '$2-'));
-  return str.replace(/(\s|_|-|A-Z)?(\w+)/g, '$2-');
+
+  return str.replace(/(\s|_|-)?([A-Z])/g, '-$2').replace(/(^-)/, '').toLowerCase();
 }
