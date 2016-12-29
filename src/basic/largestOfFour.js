ee7daafc8b640f2ce7e0dbb2ea8e58fd39de3f03
TJRobson <thomas.j.robson@hotmail.com>
2016-12-29 23:57:43 +1300

    adding my solutions for titleCase and LargestNumInArr

--- src/basic/largestOfFour.js
+++ src/basic/largestOfFour.js
@@ -1,4 +1,17 @@
 export default function largestOfFour(arr) {
-  // You can do this!
-  return arr;
+  var newArr = []
+  for(var i = 0; i < arr.length; i++) {
+    var biggest = 0
+//     console.log(biggest, 'biggest');
+    for(var j = 0; j < arr[i].length; j++) {
+      if(arr[i][j] > biggest) {
+        biggest = arr[i][j]
+      }
+      newArr[i] = biggest
+//       console.log(newArr[i], 'newArr');
+    }
+  }
+  return newArr;
 }
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
--- src/basic/titleCase.js
+++ src/basic/titleCase.js
@@ -1,3 +1,8 @@
 export default function titleCase(str) {
-  return str;
+  return str.toLowerCase().split(' ').map(function(word) {
+    console.log(word.slice(1), 'slice')
+    return word.charAt(0).toUpperCase() + word.slice(1)
+    }).join(' ')
 }
+
+titleCase("I'm a little tea pot");
